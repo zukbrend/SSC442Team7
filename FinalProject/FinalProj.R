bccc60b5805194e1ee66b5987a15309ccabf98c5
Brendan Zuk <zukbrend@msu.edu>
2020-04-24 00:14:48 -0400

    First 2 basic, exploratory graphs completed to find some possible variables with high correlations

--- FinalProject/FinalProj.R
+++ FinalProject/FinalProj.R
@@ -17,18 +17,19 @@ teamRankings <- read.csv("DataSets/Team_Year_Rankings.csv")
 # Initial Preprocessign to remove random characters added during csv conversion
 names(Metro15) <- 
   c(
-    "MetroArea",	"TotalPop",	"TotalMale",	"%TotalMale",	"TotalFemale",	
-    "%TotalFemale",	"Under5",	"%Under5",	"5to9",	"%5to9",	"10to14",	"%10to14",
-    "15to19",	"%15to19",	"20to24",	"%20to24",	"25to34",	"%25to34",	"35to44",	"%35to44",
-    "45to54",	"%45to54",	"55to59",	"%55to59",	"60to64",	"%60to64",	"65to74",	"%65to74",
-    "75to84",	"%75to84",	"85andover",	"%85andover",	"18andover",	"%18andover",	"18andoverMale",
-    "%18andoverMale",	"18andoverFemale",	"%18andoverFemale",	"65andover",	"%65andover",	
-    "65andoverMale",	"%65andoverMale",	"65andoverFemale",	"%65andoverFemale",	"Onerace",
-    "%Onerace",	"White",	"%White",	"BlackorAfricanAmerican",	"%BlackorAfricanAmerican",
-    "AmericanIndianandAlaskaNative",	"%AmericanIndianandAlaskaNative",	"Asian",	"%Asian",
-    "NativeHawaiianandOtherPacificIslander",	"%NativeHawaiianandOtherPacificIslander",
-    "HispanicLatino",	"%HispanicLatino",	"Someotherrace",	"%Someotherrace",	"Twoormoreraces",
-    "%Twoormoreraces",	"BelowPovertyLevel",	"AtAbovePovertyLevel",	"LessHighSchool",
+    "City",	"State",	"Team",	"Year",	"PositionInLeague",	"InSuperBowl",	"WonSuperBowl",
+    "MetroArea",	"TotalPop",	"TotalMale",	"PctTotalMale",	"TotalFemale",	
+    "PctTotalFemale",	"Under5",	"PctUnder5",	"5to9",	"Pct5to9",	"10to14",	"Pct10to14",
+    "15to19",	"Pct15to19",	"20to24",	"Pct20to24",	"25to34",	"Pct25to34",	"35to44",	"Pct35to44",
+    "45to54",	"Pct45to54",	"55to59",	"Pct55to59",	"60to64",	"Pct60to64",	"65to74",	"Pct65to74",
+    "75to84",	"Pct75to84",	"85andover",	"Pct85andover",	"18andover",	"Pct18andover",	"18andoverMale",
+    "Pct18andoverMale",	"18andoverFemale",	"Pct18andoverFemale",	"65andover",	"Pct65andover",	
+    "65andoverMale",	"Pct65andoverMale",	"65andoverFemale",	"Pct65andoverFemale",	"Onerace",
+    "PctOnerace",	"White",	"PctWhite",	"BlackorAfricanAmerican",	"PctBlackorAfricanAmerican",
+    "AmericanIndianandAlaskaNative",	"PctAmericanIndianandAlaskaNative",	"Asian",	"PctAsian",
+    "NativeHawaiianandOtherPacificIslander",	"PctNativeHawaiianandOtherPacificIslander",
+    "HispanicLatino",	"PctHispanicLatino",	"Someotherrace",	"PctSomeotherrace",	"Twoormoreraces",
+    "PctTwoormoreraces",	"BelowPovertyLevel",	"AtAbovePovertyLevel",	"LessHighSchool",
     "HighSchoolGraduate",	"SomeCollege/Associates",	"Bachelors+",	"Employment16to19",
     "Employment20to24",	"Employment25to29",	"Employment30to34",	"Employment35to44",
     "Employment45to54",	"Employment55to59",	"Employment60to64",	"Employment65to74",
@@ -41,6 +42,7 @@ names(Metro15) <-
     "Unemployment20to64Male",	"Unemployment20to64Female",	"UnemploymentBelowPovertyLevel",
     "UnemploymentAtAbovePovertyLevel",	"Unemployment",	"UnemploymentLessHighSchool",
     "UnemploymentHighSchool",	"UnemploymentSomeCollege/Associates",	"UnemploymentBachelors+"
+    
   )
   
 names(Metro16) <- 
@@ -240,16 +242,45 @@ names(State18) <-
   )
 
 # Initial visualizations
-Rankings15 <- teamRankings$PositionInLeague[which(teamRankings$Year == 2015)]
-
 UnempPlot <- ggplot(
-  dat = Metro15,
+  data = Metro15,
   mapping = aes(
-    x=MetroArea,
+    x=Team,
     y=Unemployment
   )
 ) +
   geom_col() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  geom_text(
+    mapping = aes(
+      label = PositionInLeague
+    ),
+    position = position_dodge(1),
+    vjust = 0
+  )
 
 UnempPlot
+
+PercentWhitePlot <- ggplot(
+  data = Metro15,
+  mapping = aes(
+    x = Team,
+    y = PctWhite
+  )
+) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  geom_text(
+    aes(
+      label = PositionInLeague
+    ),
+    position = position_dodge(1),
+    vjust = 0
+  )
+
+PercentWhitePlot
+
+
+
+
+
